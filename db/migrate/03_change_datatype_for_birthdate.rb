e805408fd3c1798b06437635335d34b3530d12b5
Chad Lengefeld <chadlengefeld@gmail.com>
2020-03-07 15:41:53 +0000

    Automatically backed up by Learn

--- db/migrate/03_change_datatype_for_birthdate.rb
+++ db/migrate/03_change_datatype_for_birthdate.rb
@@ -1,10 +1,10 @@
 class ChangeDatatypeForBirthdate < ActiveRecord::Migration[5.1]
-  
+
   def change
-    change_column :students do |t|
-      t.string :table_name
-      t.string :column_name
-      t.string :type
+    change_column :students,
+     :table_name
+      : :column_name, :string
+    
     end
   end
 
